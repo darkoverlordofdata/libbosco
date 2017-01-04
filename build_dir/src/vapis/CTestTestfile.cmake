44d28978bd40813db3ed8c013882b0e5895416d7
darkoverlordofdata <darkoverlordofdata@gmail.com>
2017-01-03 23:58:24 -0800

    remove build_dir

--- build_dir/src/vapis/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/bruce/bzr/libbosco/src/vapis
-# Build directory: /home/bruce/bzr/libbosco/build_dir/src/vapis
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
